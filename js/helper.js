7dca5be7489d9c8eb6399f9e9e741e915a9f6b8d
Filip Dam <fdamljanovic@gmail.com>
2019-03-20 15:51:10 +0100

    Different maps API was used, so the code was refactored.

--- js/helper.js
+++ js/helper.js
@@ -98,25 +98,25 @@ https://developers.google.com/maps/documentation/javascript/reference
 */
 var map;    // declares a global map variable
 
-
 /*
 Start here! initializeMap() is called when page is loaded.
 */
 function initializeMap() {
-
   var locations;
 
-  var mapOptions = {
-    disableDefaultUI: true
-  };
+  var platform = new H.service.Platform({
+    'app_id': 'l8wFI8WC2ToCOciTA4Xn',
+    'app_code': 'HA94cbPIjbqejgvmWpzn4g'
+  });
 
-  /*
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js.
-  */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+  // Obtain the default map types from the platform object
+  var maptypes = platform.createDefaultLayers();
 
-
+  // Instantiate (and display) a map object:
+  map = new H.Map(document.getElementById('map'), maptypes.normal.map,{
+    zoom: 7,
+    center: {lat: 52.53075, lng: 13.3851}
+  });
   /*
   locationFinder() returns an array of every location string from the JSONs
   written for bio, education, and work.
@@ -127,13 +127,13 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(model.bio.contacts.location);
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    model.education.schools.forEach(function(school){
       locations.push(school.location);
     });
 
@@ -141,7 +141,7 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    model.work.jobs.forEach(function(job){
       locations.push(job.location);
     });
 
@@ -149,81 +149,46 @@ function initializeMap() {
   }
 
   /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
-
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
-
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
-
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-
-    // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
-    });
-
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-  }
-
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      createMapMarker(results[0]);
-    }
-  }
-
-  /*
   pinPoster(locations) takes in the array of locations created by locationFinder()
   and fires off Google place searches for each location
   */
   function pinPoster(locations) {
+    var searchResult, error;
 
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
-
-    // Iterates through the array of locations, creates a search object for each location
-      locations.forEach(function(place){
-      // the search request object
-      var request = {
-        query: place
+    locations.forEach(function(place){
+      // Define search parameters:
+      var geocodingParams = {
+        // Plain text search for places with the word "hotel"
+        // associated with them:
+        searchText: place
       };
+      // Define a callback function to handle data on success:
+      function onResult(data) {
+        var position, marker;
+        searchResult = data.Response.View[0].Result;
+        // Add a marker for each location found
+        for (i = 0;  i < searchResult.length; i++) {
+          position = {
+            lat: searchResult[i].Location.DisplayPosition.Latitude,
+            lng: searchResult[i].Location.DisplayPosition.Longitude
+          };
+          marker = new H.map.Marker(position);
+          map.addObject(marker);
+        }
+        map.setCenter({lat: position.lat, lng:position.lng});
+      }
 
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.textSearch(request, callback);
+      // Get an instance of the geocoding service:
+      var geocoder = platform.getGeocodingService();
+
+      // Call the geocode method with the geocoding parameters,
+      // the callback and an error callback function (called if a
+      // communication error occurs):
+      geocoder.geocode(geocodingParams, onResult, function(e) {
+        alert(e);
+      });
     });
   }
-
-  // Sets the boundaries of the map based on pin locations
-  window.mapBounds = new google.maps.LatLngBounds();
 
   // locations is an array of location strings returned from locationFinder()
   locations = locationFinder();
@@ -239,11 +204,4 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
-
-// Vanilla JS way to listen for resizing of the window
-// and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('load', initializeMap);
